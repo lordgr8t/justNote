d471ddd8a48a1f2e34a79137aff84eec5dc2d6ff
lordgr8t <lordgr8t@gmail.com>
2021-10-16 22:22:24 +0300

    signIn signUp front

--- index.php
+++ index.php
@@ -12,9 +12,47 @@ if ($url == "/?signup") {
 	$showhide = "<script>$('.signup').hide();</script>";
 }
 ?>
-<body>
+<body class="welcome">
 	<?php include('includes/header.php') ?>
+	<div class="signin">
+		<div class="tac">
+			<img src="img/logo100.png" class="logo">
+		</div>
+		<div class="langpicker d-flex jcsb m0-a mt-4 mb-4">
+			<div onclick="en()" class="w-lang en active">EN</div>
+			<div onclick="ru()" class="w-lang ru">RU</div>
+		</div>
+		<div class="log-input pos-r">
+			<input type="text" placeholder="login" name="login">
+			<div class="log-next pos-a">
+				<img src="img/next.svg">
+			</div>
+		</div>
+		<div class="pass-input pos-r">
+			<input type="text" placeholder="password" name="pass">
+			<div class="log-next pos-a">
+				<img src="img/next.svg">
+			</div>
+		</div>
+		<div class="noacc link tac mt-3" onclick="open_sup()">Not have  an  accaunt?</div>
+	</div>
 
+	<div class="bluer_bg dn animate__animated"></div>
+	<div class="signup tac dn animate__animated">
+		<div class="close pointer">
+			<img onclick="close_sup()" src="img/close.svg">
+		</div>
+		<h1 class="tac mt-3 mb-5">Create account</h1>
+		<input class="mb-3" type="text" placeholder="login"><br>
+		<input class="mb-3" type="text" placeholder="mail"><br>
+		<input class="mb-3" type="text" placeholder="password"><br>
+		<input class="mb-5" type="text" placeholder="accept password"><br>
+		<div class="checker-sup">
+			<div>I agree with <a href="#">Privacy Policy</a>, <a href="#">Cookie Policy</a> and <a href="#">Terms of Use</a>.</div>
+			<input class="checkbox" type="checkbox">
+		</div>
+		<div class="link mt-3">Create account</div>
+	</div>
 
 	<?php include('includes/footer.php') ?>	
 	<?php include('includes/script.php') ?>
---
+++ js/login.js
@@ -0,0 +1,40 @@
+let winH = $(document).height();
+let winW = $(document).width();
+let signIH = $('.signin').height();
+let signUH = $('.signup').height();
+let signUW = $('.signup').width();
+$('.signup').css({'top' : winH/2-signUH/2 - 50});
+$('.signup').css({'left' : winW/2-signUW/2-15});
+$('.signin').css({'marginTop' : winH/2-signIH/2 - 50});
+
+
+function en() {
+	$('.en').addClass("active");
+	$('.ru').removeClass("active");
+};
+function ru() {
+	$('.ru').addClass("active");
+	$('.en').removeClass("active");		
+};
+$('.log-input .log-next').bind("click", function() {
+	$('.log-input .log-next img').attr('src','img/load_note.gif');	
+	setTimeout(function(){
+		$('.pass-input').addClass("pass-input-anim");
+		$('.log-input input').css({'borderRadius' : '6px 6px 0 0'});
+		$('.pass-input input').css({'borderRadius' : '0 0 6px 6px'});
+		$('.log-input .log-next').remove();
+	}, 500);	
+});
+
+function open_sup() {	
+	$('.bluer_bg, .signup').removeClass("dn");
+	$('.signup').removeClass("animate__backOutUp");			
+	$('.signup').addClass("animate__backInDown");	
+	$('.signin').css({'filter' : 'blur(4px)'});		
+}
+function close_sup() {
+	$('.signup').removeClass("animate__backInDown");
+	$('.signup').addClass("animate__backOutUp");
+	$('.bluer_bg').addClass("dn");
+	$('.signin').css({'filter' : 'blur(0px)'});	
+}
